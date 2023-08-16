8ef7d02a6cafee9cbfc33cf23252704aa4cc1b2f
Sam Poloha <samuel.poloha@gmail.com>
2023-08-16 07:51:05 +0200

    js clicking pics shadow

--- index.js
+++ index.js
@@ -17,5 +17,54 @@ $(".menu-hamburger").on("click", function () {
 
 //better with slow animation
 $(".chevron-up").on("click", function () {
-	$("html, body").animate({ scrollTop: 0 }, 2000);
+	$("html, body").animate({ scrollTop: 0 }, 1500);
+});
+
+//working on zooming and carousel once zoomed
+//first learning some basics like toggle class and toggle last clicked item back
+// $("img").on("click", function() {
+//     $(this).toggleClass("pressed")
+//         }
+//     );
+//this toggles clas "pressed" on specific element on and off with each click on img
+
+//now I need to toggle off the previous one, once new one clicked, let's play
+// $("img").click(function () {
+// 	$(this).addClass("clicked");
+// // $(".clicked").removeClass("pressed");
+// });
+
+//Bing AI helping again:
+
+// Store the previous element in a variable
+var prev = null;
+
+// Attach a click event handler to all img elements
+// $("img").click(function() {
+//   // Remove the pressed class from the previous element, if any
+//   if (prev) {
+//     prev.removeClass("pressed");
+//   }
+//   // Toggle the pressed class on the current element
+//   $(this).toggleClass("pressed");
+//   // Update the previous element to the current one
+//   prev = $(this);
+// });
+// thanks robot! ;)
+
+//updated so it works also if user clicks the same element second time in a row
+// Store the previous element in a variable
+var prev = null;
+
+// Attach a click event handler to all img elements
+$("img").click(function () {
+	// Check if the current element is the same as the previous one
+	if (prev && prev[0] !== this) {
+		// Remove the pressed class from the previous element
+		prev.removeClass("pressed");
+	}
+	// Toggle the pressed class on the current element
+	$(this).toggleClass("pressed");
+	// Update the previous element to the current one
+	prev = $(this);
 });
